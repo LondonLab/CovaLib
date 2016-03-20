bf5c19dd13c2c668f556736dbd6a9d9e7e7c2e7f
danielzaidman <daniel.zaidman@gmail.com>
2016-03-20 09:33:31 +0200

    change cluster to work with chemfarm

--- Code/Paths.py
+++ Code/Paths.py
@@ -1,3 +1,5 @@
-DOCKBASE = "/home/labs/londonir/londonir/git_dock/DOCK/"
+import os
+HOME = os.environ['HOME']
+DOCKBASE = "/work/londonlab/git_dock/DOCK/"
 ROSETTA = "/apps/RH6U4/rosetta/clang/rosetta_src_2015.02.57538_bundle/main/source/bin/"
-SCRIPTS = "~/CovaLib/Scripts/"
+SCRIPTS = HOME + "/CovaLib/Scripts/"
