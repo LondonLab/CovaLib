6488a1742f7cb2009e9452b6c5a07ae4a8522056
dinka3737 <dinka3737@gmail.com>
2016-03-22 14:43:22 +0200

    resolve stupid conflicts

--- Code/Paths.py
+++ Code/Paths.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 import os
 HOME = os.environ['HOME']
->>>>>>> 06185881f43f87a1080b80b4acf44e8d2c9af1dd
 DOCKBASE = "/work/londonlab/git_dock/DOCK/"
 ROSETTA = "/apps/RH6U4/rosetta/clang/rosetta_src_2015.02.57538_bundle/main/source/bin/"
 SCRIPTS = HOME + "/CovaLib/Scripts/"
--- Code/__init__.py
+++ Code/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-__all__ = ["DOCK_Prepare", "DOCKovalent", "PyUtils", "Cluster", "Paths", "PDBUtils","Poses_parser"]
-=======
-__all__ = ["DOCK_Prepare", "DOCKovalent", "PyUtils", "Cluster", "Paths", "Result_List"]
->>>>>>> 06185881f43f87a1080b80b4acf44e8d2c9af1dd
+__all__ = ["DOCK_Prepare", "DOCKovalent", "PyUtils", "Cluster", "Paths", "PDBUtils","Poses_parser", "Result_List"]
